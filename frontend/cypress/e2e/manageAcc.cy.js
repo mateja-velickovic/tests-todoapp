578e3c886f81081027b98f6984c7d957560ad3ae
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2025-01-10 16:31:54 +0100

    feat(e2e): Ajout d'un test pour la gestion du profile (déconnexion/modification)

--- frontend/cypress/e2e/manageAcc.cy.js
+++ frontend/cypress/e2e/manageAcc.cy.js
@@ -1,4 +1,4 @@
-describe('Se déconnecter d\'un compte', () => {
+describe('Gérer un compte', () => {
   beforeEach(() => {
     cy.visit('/register');
     cy.get('input[name="email"]').type('logout@example.com');
@@ -20,5 +20,18 @@ describe('Se déconnecter d\'un compte', () => {
     cy.contains('Déconnection').click();
 
     cy.url().should('include', '/');
+  });
+
+  it('Modifier les informations du compte', () => {
+    cy.get('img[alt="Profile"]').click({ force: true });
+
+    cy.contains('Mon Profile').click();
+
+    cy.get('input[name="name"]').type('Mateja');
+    cy.get('input[name="address"]').type('Avenue');
+    cy.get('input[name="zip"]').type('1815');
+    cy.get('input[name="location"]').type('Clarens');
+
+    cy.get('button[type="submit"]').click();
   });
 });
